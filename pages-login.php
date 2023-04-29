a41eff71ef125ee985ab508cdb9d93cd73aec208
dhirajtek13 <dhirajtek13@gmail.com>
2023-04-29 15:19:41 +0530

    struc

--- pages-login.php
+++ pages-login.php
@@ -136,32 +136,7 @@
   <script src="assets/js/main.js"></script>
 
   <script>
-
-const rmCheck = document.getElementById("rememberMe"),
-    usernameInput = document.getElementById("username");
-    passwordInput = document.getElementById("password");
-
-if (localStorage.checkbox && localStorage.checkbox !== "") {
-    rmCheck.setAttribute("checked", "checked");
-    usernameInput.value = localStorage.username;
-    passwordInput.value = localStorage.password;
-  } else {
-    rmCheck.removeAttribute("checked");
-    usernameInput.value = "";
-  }
-
-
-  function lsRememberMe() {
-    if (rmCheck.checked && usernameInput.value !== "") {
-      localStorage.username = usernameInput.value;
-      localStorage.password = passwordInput.value;
-      localStorage.checkbox = rmCheck.value;
-    } else {
-      localStorage.username = "";
-      localStorage.checkbox = "";
-      localStorage.password = "";
-    }
-  }
+    
   </script>
 
 </body>
