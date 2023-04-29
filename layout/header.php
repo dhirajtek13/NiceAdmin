e9f1e2ceff593786d37b6a64f7c301b628a106fe
dhirajtek13 <dhirajtek13@gmail.com>
2023-04-29 16:24:27 +0530

    Update header.php

--- layout/header.php
+++ layout/header.php
@@ -180,15 +180,15 @@
           </a><!-- End Profile Iamge Icon -->
 
           <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
-            <li class="dropdown-header">
+            <!-- <li class="dropdown-header">
               <h6>Kevin Anderson</h6>
               <span>Web Designer</span>
             </li>
             <li>
               <hr class="dropdown-divider">
-            </li>
+            </li> -->
 
-            <li>
+            <!-- <li>
               <a class="dropdown-item d-flex align-items-center" href="users-profile.php">
                 <i class="bi bi-person"></i>
                 <span>My Profile</span>
@@ -216,7 +216,7 @@
             </li>
             <li>
               <hr class="dropdown-divider">
-            </li>
+            </li> -->
 
             <li>
               <a class="dropdown-item d-flex align-items-center" href="/logout.php">
