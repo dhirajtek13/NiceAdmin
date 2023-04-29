39e6fc983df44f7f7ea636b77ce7d69bf7b60a46
dhirajtek13 <dhirajtek13@gmail.com>
2023-04-29 12:08:02 +0530

    db_schema added

--- db/db_schema.sql
+++ db/db_schema.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 21, 2023 at 10:17 AM
+-- Generation Time: Apr 29, 2023 at 08:35 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.0.28
 
@@ -18,30 +18,8 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `bt`
+-- Database: `niceadmin`
 --
-
--- --------------------------------------------------------
-
---
--- Table structure for table `assignees`
---
-
-CREATE TABLE `assignees` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `roll_id` int(11) NOT NULL,
-  `status` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `assignees`
---
-
-INSERT INTO `assignees` (`id`, `name`, `roll_id`, `status`) VALUES
-(1, 'No Assignee', 0, 0),
-(2, 'Amit', 1, 1),
-(3, 'Upendra', 2, 1);
 
 -- --------------------------------------------------------
 
@@ -62,7 +40,22 @@ INSERT INTO `c_status_types` (`id`, `type_name`) VALUES
 (1, 'WIP'),
 (2, 'Hold'),
 (3, 'Closed'),
-(4, 'Reassigned');
+(4, 'Reassigned'),
+(5, 'Ready For Development'),
+(6, 'Analysis'),
+(7, 'Code Review'),
+(8, 'Awaiting Customer Feedback'),
+(9, 'Reassigned Closed'),
+(10, 'Code review rework 1'),
+(11, 'Code review rework 2'),
+(12, 'Code review rework 3'),
+(13, 'Gamma Testing'),
+(14, 'Ready For Gamma'),
+(15, 'Ready For Isolated'),
+(16, 'Ready for Live'),
+(17, 'Ready to Merge'),
+(18, 'UAT Testing'),
+(19, 'Live Verify');
 
 -- --------------------------------------------------------
 
@@ -73,28 +66,16 @@ INSERT INTO `c_status_types` (`id`, `type_name`) VALUES
 CREATE TABLE `log_history` (
   `id` int(11) NOT NULL,
   `ticket_id` int(10) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
   `dates` datetime NOT NULL,
   `hrs` varchar(100) NOT NULL,
   `c_status` int(11) NOT NULL,
   `what_is_done` text NOT NULL,
   `what_is_pending` text NOT NULL DEFAULT 'NA',
-  `what_support_required` text NOT NULL DEFAULT 'NA'
+  `what_support_required` text NOT NULL DEFAULT 'NA',
+  `created_at` datetime DEFAULT current_timestamp(),
+  `updated_at` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `log_history`
---
-
-INSERT INTO `log_history` (`id`, `ticket_id`, `dates`, `hrs`, `c_status`, `what_is_done`, `what_is_pending`, `what_support_required`) VALUES
-(1, 9, '2023-04-12 00:00:00', '12', 2, 'ffslfjf sdf\r\nsfsf', ' sdfsfsdfsfds dfsfd', 'NA'),
-(2, 5, '2023-04-11 00:00:00', '11', 4, 'dgdfgdf', 'NA1', 'NA'),
-(3, 5, '2023-04-12 00:00:00', '2', 2, 'dgdfgdf', 'aajlksdjd', 'NA'),
-(4, 9, '2023-04-12 00:00:00', '11', 1, 'this is it', 'NA', 'NA'),
-(5, 9, '2023-04-12 00:00:00', '11', 3, 'this is closed', 'NA', ''),
-(6, 9, '2023-04-20 00:00:00', '11', 3, 'again closed', 'NA', 'NA'),
-(7, 9, '2023-04-12 00:00:00', '11', 3, 'closss', 'NA', 'NA'),
-(8, 9, '2023-04-12 00:00:00', '11', 3, 'this', 'NA', 'NA'),
-(9, 0, '2023-04-12 00:00:00', '12', 4, 'ss11', 'NA11', 'NA11');
 
 -- --------------------------------------------------------
 
@@ -105,7 +86,11 @@ INSERT INTO `log_history` (`id`, `ticket_id`, `dates`, `hrs`, `c_status`, `what_
 CREATE TABLE `log_timing` (
   `id` int(11) NOT NULL,
   `ticket_id` int(11) NOT NULL,
-  `status` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `assignee_id` int(10) DEFAULT NULL,
+  `c_status` int(11) NOT NULL,
+  `activity_type` varchar(200) DEFAULT NULL,
+  `details` text DEFAULT NULL,
   `datetime` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -128,26 +113,9 @@ CREATE TABLE `tickets` (
   `actual_end_date` datetime DEFAULT NULL,
   `planned_hrs` float(11,2) DEFAULT NULL,
   `actual_hrs` float(11,2) DEFAULT NULL,
-  `created_at` datetime DEFAULT current_timestamp(),
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   `updated_at` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `tickets`
---
-
-INSERT INTO `tickets` (`id`, `ticket_id`, `type_id`, `c_status`, `assignee_id`, `assigned_date`, `plan_start_date`, `plan_end_date`, `actual_start_date`, `actual_end_date`, `planned_hrs`, `actual_hrs`, `created_at`, `updated_at`) VALUES
-(1, '16750', 2, '2', 4, '2023-04-01 20:58:30', '2023-04-02 20:58:41', '2023-04-03 20:58:47', '2023-04-04 20:58:51', '2023-04-05 20:58:56', 20.20, 11.50, NULL, '2023-04-21 12:26:46'),
-(5, '6768', 2, '2', 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 11.00, 2.00, NULL, '2023-04-21 11:43:42'),
-(7, '545345', 2, '1', 1, '2023-04-18 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '2023-04-24 00:00:00', '0000-00-00 00:00:00', 0.00, 0.00, NULL, '2023-04-21 11:43:42'),
-(8, '234234', 1, '1', 1, '2023-04-12 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0.00, 0.00, NULL, '2023-04-21 11:43:42'),
-(9, 'TCI-123', 1, '1', 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 75.00, 100.00, NULL, '2023-04-21 11:43:42'),
-(10, '11zx', 1, '1', 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0.00, 0.00, NULL, '2023-04-21 11:43:42'),
-(11, 'ssds', 1, '1', 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0.00, 0.00, NULL, '2023-04-21 11:43:42'),
-(12, '3434345', 1, '1', 4, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 11.00, 4.00, '2023-04-21 12:04:21', '2023-04-21 12:04:21'),
-(13, 'ee', 1, '1', 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0.00, 0.00, '2023-04-21 12:46:29', '2023-04-21 12:46:29'),
-(14, 'xxx', 1, '1', 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0.00, 0.00, '2023-04-21 12:46:47', '2023-04-21 12:46:47'),
-(15, 'asg', 2, '1', 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 11.00, 2.00, '2023-04-21 13:05:32', '2023-04-21 13:05:32');
 
 -- --------------------------------------------------------
 
@@ -166,17 +134,64 @@ CREATE TABLE `ticket_types` (
 
 INSERT INTO `ticket_types` (`id`, `type_name`) VALUES
 (1, 'Bug'),
-(2, 'Story');
+(2, 'Story'),
+(3, 'Task'),
+(4, 'Integration');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) NOT NULL,
+  `username` varchar(200) NOT NULL,
+  `fname` varchar(255) DEFAULT NULL,
+  `lname` varchar(255) DEFAULT NULL,
+  `user_type` int(10) NOT NULL COMMENT '1:PM,\r\n2:TL,\r\n3:Dev,\r\n4:QA',
+  `employee_id` int(10) NOT NULL,
+  `designation` varchar(100) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `fname`, `lname`, `user_type`, `employee_id`, `designation`, `email`, `password`, `created_at`, `updated_at`) VALUES
+(1, 'dhirajtekade', 'Dhiraj', 'Tekade', 3, 4762, 'full s', 'dhirajtek13@gmail.com', '$2y$10$R8NtjFXOLjDglT7wOVL14utYQJQFJPQxFws4t0WBCRX24aSal2dtG', '2023-04-25 17:32:13', '2023-04-28 17:17:43'),
+(2, 'vumesh', 'Umesh', 'Verma', 1, 111, 'PM', 'vumesh@espire.com', '$2y$10$/pVJVAzUYkMsjEC0frHReOOeAqyzsSXqVZRsFonzPVg8Iz7Z0Ovky', '2023-04-25 20:52:17', '2023-04-25 20:52:17'),
+(3, 'kamit', 'Amit', 'Karki', 2, 222, 'LE', 'kamit@espire.com', '$2y$10$/pVJVAzUYkMsjEC0frHReOOeAqyzsSXqVZRsFonzPVg8Iz7Z0Ovky', '2023-04-25 20:54:53', '2023-04-25 20:54:53'),
+(4, 'upendra', 'Upendra', 'Prasad', 3, 4555, 'lead eng', 'u@email.com', '$2y$10$a0nE4VkNRE5qZS/sLXTjsOzs2mNrKb/d/x4ua1qK.sNzfg.1K61IS', '2023-04-27 13:36:54', '2023-04-27 13:36:54');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_type`
+--
+
+CREATE TABLE `user_type` (
+  `id` int(10) NOT NULL,
+  `type_name` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user_type`
+--
+
+INSERT INTO `user_type` (`id`, `type_name`) VALUES
+(1, 'PM'),
+(2, 'TL'),
+(3, 'Dev'),
+(4, 'QA');
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `assignees`
---
-ALTER TABLE `assignees`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `c_status_types`
@@ -209,26 +224,33 @@ ALTER TABLE `ticket_types`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- Indexes for table `users`
 --
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
 
 --
--- AUTO_INCREMENT for table `assignees`
+-- Indexes for table `user_type`
 --
-ALTER TABLE `assignees`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+ALTER TABLE `user_type`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
 
 --
 -- AUTO_INCREMENT for table `c_status_types`
 --
 ALTER TABLE `c_status_types`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `log_history`
 --
 ALTER TABLE `log_history`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `log_timing`
@@ -240,13 +262,25 @@ ALTER TABLE `log_timing`
 -- AUTO_INCREMENT for table `tickets`
 --
 ALTER TABLE `tickets`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `ticket_types`
 --
 ALTER TABLE `ticket_types`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `user_type`
+--
+ALTER TABLE `user_type`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
