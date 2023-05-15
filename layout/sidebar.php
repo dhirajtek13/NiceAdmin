58b5c4346610c94526749b0d3418d174538da34d
dhirajtek13 <dhirajtek13@gmail.com>
2023-05-15 21:31:53 +0530

    sidebar added

--- layout/sidebar.php
+++ layout/sidebar.php
@@ -13,117 +13,99 @@
 
       <li class="nav-heading">Pages</li>
 
-      <!-- <li class="nav-item">
-        <a class="nav-link collapsed" href="users-profile.php">
-          <i class="bi bi-person"></i>
-          <span>Profile</span>
-        </a>
-      </li> -->
-      <!-- End Profile Page Nav -->
+      <?php if(isset($_SESSION['user_type']) && $_SESSION['user_type'] == 1) { ?>
+        
+        <!-- Ticket management -->
+        <li class="nav-item">
+          <a class="nav-link collapsed" href="tickets.php">
+          <i class="bi bi-layout-text-window-reverse"></i>
+            <span>Ticket Management</span>
+          </a>
+        </li>
 
-      <?php 
-        if(isset($_SESSION['user_type']) && $_SESSION['user_type'] == 1) {
-          echo '<li class="nav-item">
-            <a class="nav-link collapsed" href="add-user.php">
-              <i class="bi bi-card-list"></i>
-              <span>Add User</span>
-            </a>
-          </li>';
-        } 
-      ?>
-      <!-- End Register Page Nav -->
+        <!-- Project management -->
+        <li class="nav-item">
+          <a class="nav-link collapsed" href="project_managment.php">
+          <i class="bi bi-layout-text-window-reverse"></i>
+            <span>Project Management</span>
+          </a>
+        </li>
 
-      <!-- End Login Page Nav -->
+        <!-- KPI management -->
+        <li class="nav-item">
+          <a class="nav-link collapsed" href="kpi_managment.php">
+          <i class="bi bi-layout-text-window-reverse"></i>
+            <span>KPI Master</span>
+          </a>
+        </li>
 
-      <?php 
-        // if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true) {
-          // echo '<li class="nav-item">
-          //   <a class="nav-link collapsed" href="pages-login.php">
-          //     <i class="bi bi-box-arrow-in-right"></i>
-          //     <span>Login</span>
-          //   </a>
-          // </li>';
-        // } 
-      ?>
+        <!-- Configuration Tab -->
+        <li class="nav-item">
+          <a class="nav-link collapsed" href="configuration.php">
+          <i class="bi bi-layout-text-window-reverse"></i>
+            <span>Configuration Tab</span>
+          </a>
+        </li>
 
-      
+        <!-- Master -->
+        <li class="nav-item">
+          <a class="nav-link " data-bs-target="#master-nav" data-bs-toggle="collapse" href="#">
+            <i class="bi bi-menu-button-wide"></i><span>Master</span><i class="bi bi-chevron-down ms-auto"></i>
+          </a>
+          <ul id="master-nav" class="nav-content collapse" data-bs-parent="#sidebar-nav">
+            <li class="nav-item">
+              <a class="nav-link collapsed" href="master_cstatus.php">
+                <i class="bi bi-card-list"></i>
+                <span>Ticket Status Master</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link collapsed" href="master_tickettypes.php">
+                <i class="bi bi-card-list"></i>
+                <span>Ticket Type Master</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link collapsed" href="master_usertypes.php">
+                <i class="bi bi-card-list"></i>
+                <span>User Type Master</span>
+              </a>
+            </li>
+          </ul>
+        </li>
 
-      <?php 
-        if(isset($_SESSION['user_type']) && $_SESSION['user_type'] == 1) {
-          echo '<li class="nav-item">
-            <a class="nav-link collapsed" href="tickets.php">
-            <i class="bi bi-circle"></i>
-              <span>Ticket Management</span>
-            </a>
-          </li>';
-        } 
-      ?>
+        <!-- User management -->
+        <li class="nav-item">
+          <a class="nav-link " data-bs-target="#user-nav" data-bs-toggle="collapse" href="#">
+            <i class="bi bi-person"></i><span>User Management</span><i class="bi bi-chevron-down ms-auto"></i>
+          </a>
+          <ul id="user-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+            <li class="nav-item">
+              <a class="nav-link collapsed" href="add-user.php">
+                <i class="bi bi-card-list"></i>
+                <span>Add User</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link collapsed" href="user_management.php">
+              <i class="bi bi-circle"></i>
+                <span>View Users</span>
+              </a>
+            </li>
+          </ul>
+        </li>
 
-      <?php 
-        if(isset($_SESSION['user_type']) && $_SESSION['user_type'] == 1) {
-          echo '<li class="nav-item">
-            <a class="nav-link collapsed" href="monthly_log_status.php">
-            <i class="bi bi-circle"></i>
-              <span>Monthly Log Report</span>
-            </a>
-          </li>';
-        } 
-      ?>
+      <?php } ?>
 
-      <?php 
-        if(isset($_SESSION['user_type']) && $_SESSION['user_type'] == 1) {
-          echo '<li class="nav-item">
-            <a class="nav-link collapsed" href="user_management.php">
-            <i class="bi bi-circle"></i>
-              <span>User Managment</span>
-            </a>
-          </li>';
-        } 
-      ?>
-
-    <?php 
-        if(isset($_SESSION) && $_SESSION['user_type'] != 1) {
-          echo '<li class="nav-item">
-            <a class="nav-link collapsed" href="pages-ticket.php">
-              <i class="bi bi-card-list"></i>
-              <span>Tickets</span>
-            </a>
-          </li>';
-        } 
-      ?>
-
-      <?php 
-        if(isset($_SESSION) && $_SESSION['user_type'] == 1) {
-          echo '<li class="nav-item">
-            <a class="nav-link collapsed" href="master_cstatus.php">
-              <i class="bi bi-card-list"></i>
-              <span>Ticket Status Master</span>
-            </a>
-          </li>';
-        } 
-      ?>
-
-    <?php 
-        if(isset($_SESSION) && $_SESSION['user_type'] == 1) {
-          echo '<li class="nav-item">
-            <a class="nav-link collapsed" href="master_tickettypes.php">
-              <i class="bi bi-card-list"></i>
-              <span>Ticket Type Master</span>
-            </a>
-          </li>';
-        } 
-      ?>
-
-    <?php 
-        if(isset($_SESSION) && $_SESSION['user_type'] == 1) {
-          echo '<li class="nav-item">
-            <a class="nav-link collapsed" href="master_usertypes.php">
-              <i class="bi bi-card-list"></i>
-              <span>User Type Master</span>
-            </a>
-          </li>';
-        } 
-      ?>
+      <?php if(isset($_SESSION['user_type']) && $_SESSION['user_type'] != 1) { ?>
+        <li class="nav-item">
+          <a class="nav-link collapsed" href="pages-ticket.php">
+            <i class="bi bi-card-list"></i>
+            <span>Tickets</span>
+          </a>
+        </li>
+      <?php } ?>
+   
 
     </ul>
 
